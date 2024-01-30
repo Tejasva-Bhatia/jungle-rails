a14b8544cba6890f9ace220c1ef9f7c17e3693a8
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2024-01-30 00:54:53 -0600

    Admin categories feature implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
   get 'about/index'
   get 'about', to: 'about#index', as: 'about'
 
@@ -19,6 +24,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resource :dashboard, only: [:show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
